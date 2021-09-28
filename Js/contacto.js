fcc28931ac2ce023b656979bb93cf6841c8c8baa
Panchez <rauljaciel_91@hotmail.com>
2021-09-28 17:48:46 -0500

    activar/desactivar enviar

--- Js/contacto.js
+++ Js/contacto.js
@@ -1,4 +1,23 @@
 const $form = document.querySelector('#form');
+const $nombre = document.querySelector('#name');
+const $tel = document.querySelector('#phone');
+const $email = document.querySelector('#email');
+const $mensaje = document.querySelector('#message');
+const $parr = document.querySelector('#advertencia');
+const $enviar = document.querySelector('#enviar');
+
+
+
+
+$form.addEventListener('keydown', e => {
+    if ($nombre.value.length >= 1) {
+        $enviar.removeAttribute("disabled");
+
+    } else if ($nombre.value.length < 1) {
+        $enviar.setAttribute("disabled", "");
+    }
+
+})
 
 $form.addEventListener('submit', handleSubmit);
 
