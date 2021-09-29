3dd6fae17ae0cb2355b9b582c73e5bb5a5bf82e5
Panchez <rauljaciel_91@hotmail.com>
2021-09-28 20:30:56 -0500

    validacion y refrescar pagina

--- Js/contacto.js
+++ Js/contacto.js
@@ -7,19 +7,52 @@ const $parr = document.querySelector('#advertencia');
 const $enviar = document.querySelector('#enviar');
 
 
+
+
+
+
 //validar formulario
 
 $form.addEventListener('keydown', e => {
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    let regextel = /^([0-9])*$/;
 
-    if (regexEmail.test($email.value) == true && $nombre.value.length >= 1 && $tel.value.length >= 1 && $mensaje.value.length >= 1) {
+    if (regexEmail.test($email.value) == true && $nombre.value.length >= 1 && regextel.test($tel.value) == true && $tel.value.length >= 1 && $mensaje.value.length >= 1) {
         $enviar.removeAttribute("disabled");
 
-    } else if (regexEmail.test($email.value) == false || $nombre.value.length < 1 || $tel.value.length < 1 || $mensaje.value.length < 1) {
+    } else {
         $enviar.setAttribute("disabled", "");
     }
 
 })
+
+
+$email.addEventListener('keydown', e => {
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    if (regexEmail.test($email.value) == false) {
+        $email.classList.add("error");
+    } else {
+        $email.classList.remove("error");
+    }
+})
+
+$mensaje.addEventListener('keydown', e => {
+    if ($mensaje.value.length < 2) {
+        $mensaje.classList.add("error");
+    } else {
+        $mensaje.classList.remove("error");
+    }
+})
+
+$tel.addEventListener('keydown', e => {
+    let regextel = /^([0-9])*$/;
+    if (regextel.test($tel.value) == false) {
+        $tel.classList.add("error");
+    } else {
+        $tel.classList.remove("error");
+    }
+})
+
 
 //enviar formulario
 
@@ -38,6 +71,7 @@ async function handleSubmit(event) {
 
     if (response.ok) {
         this.reset();
+        location.reload();
         alert('Gracias por contactarme, te escribiré pronto');
     }
 
