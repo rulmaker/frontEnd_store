10415e335184586be81b8ea10474ab7995d9d8f5
Panchez <rauljaciel_91@hotmail.com>
2021-10-07 12:53:50 -0500

    crea elemento y guarda en localstorage

--- Js/crearProducto.js
+++ Js/crearProducto.js
@@ -1,92 +1,4 @@
-/* function addItem(item) {
-    const itemHTML =
-        "<div >" +
-        '<div class="producto" ">\n' +
-        '    <img src="' +
-        item.img +
-        '" class="procucto__imagen" alt="image">\n' +
-        '    <div class="card-body">\n' +
-        '    <h3 class="card-title">' +
-        item.name +
-        "</h5>\n" +
-        '    <p class="card-text">' +
-        item.description +
-        "</p>\n" +
-        "    </div>\n" +
-        '<a href="#" class="btn btn-primary">Añadir al carrito</a>\n' +
-        "</div>" +
-        "</div>";
-    const itemsContainer = document.getElementById("list-items");
-    itemsContainer.innerHTML += itemHTML;
-} */
-
-/* addItem({
-    name: "Camisa VUEJS",
-    img: "img/1.jpg",
-    description:
-        "Demuestra tus habilidades de VueJs con esta camisa cool. Sé el alma de la oficina.",
-});
-
-addItem({
-    name: "Camisa ANGULARJS",
-    img: "img/2.jpg",
-    description:
-        "Sé la envidia de todos tus amigos con la playera ANGULARJS, que te hará parecer todo un hacker del pentágono.",
-});
-
-addItem({
-    name: "Camisa REACTJS",
-    img: "img/3.jpg",
-    description:
-        "Destaca entre la multitud con la playera REACT JS, que te hará parecer un genio.",
-});
-
-addItem({
-    name: "Redux",
-    img: "img/4.jpg",
-    description:
-        "Con nuestra playera Redux más de una persona se te quedará mirando en la calle.",
-});
-
-addItem({
-    name: "NODEJS",
-    img: "img/5.jpg",
-    description:
-        "Con nuestra playera NODEJS parecerás un desarrollador Senior, serás la envidia de toda la oficina.",
-});
-
-addItem({
-    name: "HTML5",
-    img: "img/7.jpg",
-    description:
-        "Si eres amante de HTML, esta playera es para ti. Apta para salir a la salir a las tortillas o a los XV de tu prima.",
-});
-
-addItem({
-    name: "GitHub",
-    img: "img/8.jpg",
-    description:
-        "Solo para desarrolladores hipsters. Se vale salir a lucirla.",
-});
-
-addItem({
-    name: "TypeScript",
-    img: "img/10.jpg",
-    description: "Para los geeks que se atreven a mostrar su verdadero yo.",
-});
-
-addItem({
-    name: "JavaScript",
-    img: "img/12.jpg",
-    description:
-        "Para los que aman el front-end y el back-end por igual. Si la usas no pasarás desapercibido.",
-});
-
-addItem({
-    name: "GraphQL",
-    img: "img/13.jpg",
-    description: "Se vale salir a la calle nomás para lucirla.",
-}); */
+let arrayProductos = [];
 
 class Product {
     constructor(name, image, description, talla, price,) {
@@ -99,26 +11,6 @@ class Product {
     }
 
 }
-
-/* <div class="card text-center mb-4">
-            <div class="card-body">
-                <strong>Product</strong>: ${product.name}
-                <strong>Product Price</strong>: ${product.price}
-                <strong>Product Year</strong>: ${product.year}
-                <a href="#" class="btn btn-danger" name="delete">Delete</a>
-            </div>
-        </div>
-        
-        <div>
-        <div class="producto">
-        <img src="${product.image}" class="producto__imagen" alt="imagen">
-        <div class="card-body">
-        <h5 class="card-title">${product.name}</h5>
-        <p class="card-text">${product.description}</p>
-        
-        </div>
-        </div>
-        </div>*/
 
 class UI {
     addProduct(product) {
@@ -141,6 +33,29 @@ class UI {
         </div>`;
         productList.appendChild(element);
 
+    }
+
+    crearItem(product) {
+        let item = {
+            nombre: product.name,
+            imagen: product.image,
+            descripcion: product.description,
+            talla: product.talla,
+            precio: product.price,
+            ID: this.newID()
+        }
+
+        arrayProductos.push(item);
+
+        return arrayProductos;
+    }
+
+
+    newID() {
+        let lastID = localStorage.getItem("lastID") || "-1";
+        let newNumberID = JSON.parse(lastID) + 1;
+        localStorage.setItem("lastID", JSON.stringify(newNumberID));
+        return newNumberID
     }
 
     resetForm() {
@@ -170,6 +85,13 @@ class UI {
     }
 }
 
+
+//FUNCTIONS
+
+const guardarDB = () => {
+    localStorage.setItem('productos', JSON.stringify(arrayProductos));
+}
+
 //DOM EVENTS
 document.getElementById('product-form')
     .addEventListener('submit', function (e) {
@@ -189,6 +111,10 @@ document.getElementById('product-form')
         }
 
         ui.addProduct(product);
+
+        ui.crearItem(product);
+
+        guardarDB();
 
         ui.resetForm();
 
